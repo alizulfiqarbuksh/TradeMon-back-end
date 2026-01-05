1bd9319c2b62f8ee7152fd8b43e88b54ce62d78c
DairiNooh <dairinooh@gmail.com>
2026-01-05 13:03:43 +0300

    Added the pokemon.js model

---
+++ models/pokemon.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const pokemonSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+
+  type: {
+    type: String,
+    required: true,
+  },
+
+  level: {
+    type: Number,
+    default: 1,
+  },
+
+  shiny: {
+    type: Boolean,
+    default: false,
+  },
+
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+});
+
+pokemonSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+const Pokemon = mongoose.model('Pokemon', pokemonSchema);
+
+module.exports = Pokemon;
