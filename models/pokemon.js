38587c718673ebd6530bfb68c3d5f3455dbc5e4b
DairiNooh <dairinooh@gmail.com>
2026-01-06 11:12:34 +0300

    Fixed the pokemon model schema json part

--- models/pokemon.js
+++ models/pokemon.js
@@ -30,8 +30,6 @@ const pokemonSchema = mongoose.Schema({
 
 pokemonSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
     delete returnedObject.__v;
   },
 });
